0ea1e58520616d72367fb427167379ac3fa7074b
Eduardo Cuomo <reduardo7@gmail.com>
2022-10-30 15:47:28 -0300

    BT working - no delay

--- Main.h
+++ Main.h
@@ -8,7 +8,6 @@ class Main {
     static Motor motorLeft;
     static Motor motorRight;
     static bool btConnected;
-    static unsigned long btCycle;
 
   public:
     static void setup() {
@@ -20,65 +19,60 @@ class Main {
     }
 
     static void loop() {
-      unsigned long m = millis();
+      char bt;
 
-      if (btCycle < m) {
-        btCycle = m + 100;
-        char bt;
-
-        if (Serial.available()) {
-          btConnected = true;
-          bt = Serial.read();
-          Serial.println(bt);
-        } else {
-          // BT disconnected
-          if (btConnected) {
-            btConnected = false;
-            bt = 'S';
-            Serial.println('BT disconnected!');
-          }
+      if (Serial.available()) {
+        btConnected = true;
+        bt = Serial.read();
+        Serial.println(bt);
+      } else {
+        // BT disconnected
+        if (btConnected) {
+          btConnected = false;
+          bt = 'S';
+          Serial.println('BT disconnected!');
         }
+      }
 
-        switch (bt) {
-          case 'F':
-            // move forward(all motors rotate in forward direction)
-            motorLeft.forward();
-            motorRight.forward();
-            break;
-        
-          case 'B':
-            // move reverse (all motors rotate in reverse direction)
-            motorLeft.backward();
-            motorRight.backward();
-            break;
-        
-          case 'L':
-            // turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
-            motorLeft.backward();
-            motorRight.forward();
-            break;
-        
-          case 'R':
-            // turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
-            motorLeft.forward();
-            motorRight.backward();
-            break;
+      switch (bt) {
+        case 'F':
+          // move forward(all motors rotate in forward direction)
+          motorLeft.forward();
+          motorRight.forward();
+          break;
+      
+        case 'B':
+          // move reverse (all motors rotate in reverse direction)
+          motorLeft.backward();
+          motorRight.backward();
+          break;
+      
+        case 'L':
+          // turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
+          motorLeft.backward();
+          motorRight.forward();
+          break;
+      
+        case 'R':
+          // turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
+          motorLeft.forward();
+          motorRight.backward();
+          break;
 
-          // case 'W':
-          //   // turn led on
-          //   digitalWrite(9,HIGH);
-          //   break
-          // case 'w':
-          //   // turn led off
-          //   digitalWrite(9,LOW);
-          //   break;
-        
-          case 'S':
-            // STOP (all motors stop)
-            motorLeft.stop();
-            motorRight.stop();
-            break;
-        }
+        // case 'W':
+        //   // turn led on
+        //   digitalWrite(9,HIGH);
+        //   break
+        // case 'w':
+        //   // turn led off
+        //   digitalWrite(9,LOW);
+        //   break;
+      
+        case 'S':
+          // STOP (all motors stop)
+          motorLeft.stop();
+          motorRight.stop();
+          break;
       }
     }
 };
@@ -86,6 +80,5 @@ class Main {
 Motor Main::motorLeft(PIN_MOTOR_11, PIN_MOTOR_12, PIN_MOTOR_13, PIN_MOTOR_14);
 Motor Main::motorRight(PIN_MOTOR_21, PIN_MOTOR_22, PIN_MOTOR_23, PIN_MOTOR_24);
 bool Main::btConnected = false;
-unsigned long Main::btCycle = 0;
 
 #endif
