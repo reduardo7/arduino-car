4d49a4ced6a25fac6fb29a4af3a573c68ec9d475
Eduardo Cuomo <reduardo7@gmail.com>
2022-11-19 16:35:16 -0300

    Fix motor L&R reference

--- Config.h
+++ Config.h
@@ -4,19 +4,17 @@
 #define PIN_BT_RX 0
 #define PIN_BT_TX 1
 
-#define PIN_MOTOR_L1 2
-#define PIN_MOTOR_L2 4
-#define PIN_MOTOR_LE 3
+#define PIN_MOTOR_L1 6
+#define PIN_MOTOR_L2 7
+#define PIN_MOTOR_LE 5
 
-#define PIN_MOTOR_R1 6
-#define PIN_MOTOR_R2 7
-#define PIN_MOTOR_RE 5
+#define PIN_MOTOR_R1 2
+#define PIN_MOTOR_R2 4
+#define PIN_MOTOR_RE 3
 
 #define PIN_LED_FRONT 13
 #define PIN_LED_BACK 12
 #define PIN_HORN 11
 #define PIN_EXTRA 10
-
-#define SPEED_TURN 70
 
 #endif
